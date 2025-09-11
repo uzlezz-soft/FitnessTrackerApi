df5474b7225185cfdc3c638e6f98d60e67c3269a
Gleb Tsiushkevich <tsushkevichgleb@gmail.com>
2025-09-12 00:45:24 +0300

    feat: cache config

---
+++ FitnessTrackerApi/Configs/CacheConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace FitnessTrackerApi.Configs;
+
+public class CacheConfig
+{
+    public required int WorkoutCacheSeconds { get; set; }
+    public required int WorkoutSearchCacheSeconds { get; set; }
+    public required int RefreshTokenCacheMinutes { get; set; }
+}
